67018576a0ebcdeecfc2e6345c1fac69529d195d
sanketmohod <mohodsanket45gmail.com>
2023-12-04 12:50:50 +0530

    updated

--- Java_Practice/src/com/practice/java/Practice.java
+++ Java_Practice/src/com/practice/java/Practice.java
@@ -2,6 +2,14 @@ package com.practice.java;
 
 public class Practice {
 public static void main(String[] args) {
-	System.out.println("This is my practice java project.");
+	for(int i=1;i<=10;i++) {
+		for(int j=1;j<=10;j++) {
+			if(i>=j) {
+			System.out.print("*");
+		}
+		}
+		System.out.println();
+	
+}
 }
 }
